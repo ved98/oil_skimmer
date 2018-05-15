ba0d0ee3e45501da5d2e6c380223fa34547491a1
ved98 <vedantaggarwal27@gmail.com>
2018-05-15 10:28:07 +0000

    code updated

--- speedtest.py
+++ speedtest.py
@@ -1,7 +1,7 @@
 import os     #importing os library so as to communicate with the system
 import time   #importing time library to make Rpi wait because its too impatient 
-os.system ("sudo pigpiod") #Launching GPIO library
-time.sleep(1) # As i said it is too impatient and so if this delay is removed you will get an error
+#os.system ("sudo pigpiod") #Launching GPIO library
+#time.sleep(1) # As i said it is too impatient and so if this delay is removed you will get an error
 import pigpio
 
 ESC1=4
@@ -16,12 +16,16 @@ min_value = 700
 print("enter speed between 700 to 2000")
 while True:
     inp = raw_input()
-    if inp == 0:
-        pi.set_servo_pulsewidth(ESC1, 0)
-        pi.set_servo_pulsewidth(ESC2, 0)
-    	pi.stop()
-    elif inp>=700 and inp<=2000:
-        pi.set_servo_pulsewidth(ESC1,inp)
-        pi.set_servo_pulsewidth(ESC2,inp)
-    else:
-    	break
+    print("your inp %d" , inp )
+##    if inp == 0:
+##        print(1)
+##        pi.set_servo_pulsewidth(ESC1, 0)
+##        pi.set_servo_pulsewidth(ESC2, 0)
+##    	pi.stop()
+##    if inp>=700 and inp<=2000:
+    print(2)
+    pi.set_servo_pulsewidth(ESC1,inp)
+    pi.set_servo_pulsewidth(ESC2,inp)
+##    else:
+##        print(3)
+##    	break
