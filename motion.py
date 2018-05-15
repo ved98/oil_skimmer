5b8bfe7de61075388f6188b927f5f5536acea979
ved98 <vedantaggarwal27@gmail.com>
2018-05-15 13:41:04 +0000

    motion updated

--- motion.py
+++ motion.py
@@ -4,52 +4,55 @@ import pigpio
 import RPi.GPIO as GPIO
 import time
 
-pi = pigpio.pi();
-GPIO.setmode(GPIO.BOARD)
-
 ESC1=4
 ESC2=17 
 servo = 3
 
+pi = pigpio.pi();
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(servo, GPIO.OUT)
+
+
+
 front =7.5
-left=5
-right=10
+left=5.0
+right=10.0
 fspeed =1300
 tspeed=1000
-
+p = GPIO.PWM(servo, 50)
 GPIO.setup(servo, GPIO.OUT)
 pi.set_servo_pulsewidth(ESC1, 0)
 pi.set_servo_pulsewidth(ESC2, 0)
 p.start(front)
 
-def left():
+def LEFT():
 	p.ChangeDutyCycle(left)
 	pi.set_servo_pulsewidth(ESC1, tspeed)
 	pi.set_servo_pulsewidth(ESC2, tspeed)
 
-def right():
+def RIGHT():
 	p.ChangeDutyCycle(right)
 	pi.set_servo_pulsewidth(ESC1, tspeed)
 	pi.set_servo_pulsewidth(ESC2, tspeed)
 
-def stop():
+def STOP():
 	p.ChangeDutyCycle(front)
 	pi.set_servo_pulsewidth(ESC1, 0)
 	pi.set_servo_pulsewidth(ESC2, 0)
 
-def front():
+def FRONT():
 	p.ChangeDutyCycle(front)
 	pi.set_servo_pulsewidth(ESC1, fspeed)
 	pi.set_servo_pulsewidth(ESC2, fspeed)
 
-while true:
+while True:
 	inp = raw_input()
 	if inp == "w":
-	    front()
+	    FRONT()
 	elif inp == "a":
-	    left()
+	    LEFT()
 	elif inp == "d":
-	    right()
+	    RIGHT()
 	elif inp == "s":
-	    stop()
+	    STOP()
 	
