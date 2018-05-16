289dc32ff47debb1aee3c080086970950569e715
Vedant Aggarwal <vedantaggarwal27@gmail.com>
2018-05-16 14:10:13 +0530

    camera integrated

--- code.py
+++ code.py
@@ -7,9 +7,38 @@ import cv2
 import sys
 import numpy as np
 import skimage.measure
+import picamera
+import io
 
-def readImage(): 
-    img = cv2.imread("test.jpg", 1) # load image, 1 means load in RGB format
+ESC1=18
+ESC2=17 
+servo = 3
+camera =8 
+
+stream = io.BytesIO()
+pi = pigpio.pi();
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(servo, GPIO.OUT)
+GPIO.setup(camera, GPIO.OUT)
+
+
+front =7.5
+left=5.0
+right=10.0
+fspeed =1300
+tspeed=1000
+
+p = GPIO.PWM(servo, 50)
+c = GPIO.PWM(camera, 50)
+pi.set_servo_pulsewidth(ESC1, 0)
+pi.set_servo_pulsewidth(ESC2, 0)
+p.start(front)
+c.start(front)
+
+def readImage(stream): 
+    data=np.fromstring(stream.getvalue(), dtype=np.uint8)
+    img=cv2.imdecode(data,1)
+    #img = cv2.imread("test.jpg", 1) # load image, 1 means load in RGB format
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) # convert to hsv
 
     lower_range = np.array([0, 0, 150], dtype=np.uint8) #lower bound of color to detect
@@ -51,36 +80,11 @@ def readImage():
     # cv2.imshow('image', img)
 
     # while(1):
-    # 	k = cv2.waitKey(0)
-    # 	if(k == 27):
-    # 		break
+    #   k = cv2.waitKey(0)
+    #   if(k == 27):
+    #       break
 
     # cv2.destroyAllWindows()
-
-
-ESC1=18
-ESC2=17 
-servo = 3
-camera =8 
-
-pi = pigpio.pi();
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(servo, GPIO.OUT)
-GPIO.setup(camera, GPIO.OUT)
-
-
-front =7.5
-left=5.0
-right=10.0
-fspeed =1300
-tspeed=1000
-
-p = GPIO.PWM(servo, 50)
-c = GPIO.PWM(camera, 50)
-pi.set_servo_pulsewidth(ESC1, 0)
-pi.set_servo_pulsewidth(ESC2, 0)
-p.start(front)
-c.start(front)
 
 def LEFT():
 	p.ChangeDutyCycle(left)
@@ -104,17 +108,21 @@ def FRONT():
 
 angles = np.array([7.5,8.5,9.5,10.5,11.5,12.5,6.5,5.5,4.5,3.5,2.5])
 ptr=0
-while True:
-    print("angle="+str((angles[ptr]-2.5)*18-90))
-    c.ChangeDutyCycle(angles[ptr])
-    time.sleep(1)
-    os.system("raspistill -v -o test.jpg")
-    res=readImage()
-    if res==0:
-        FRONT()
-    else :
-        ptr+=1
-    if ptr == len(angles):
-        ptr=0
+with picamera.PiCamera() as cam1:
+    cam1.resolution = (200,200)
+    cam1.start_preview()
+    time.sleep(2)
+    while True:
+        print("angle="+str((angles[ptr]-2.5)*18-90))
+        c.ChangeDutyCycle(angles[ptr])
+        time.sleep(0.5)
+        cam1.capture(stream , format = 'jpeg')
+        res=readImage(stream)
+        if res==0:
+            FRONT()
+        else :
+            ptr+=1
+        if ptr == len(angles):
+            ptr=0
 	
 	
