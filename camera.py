5844b271c2c2d76ce708d1342a8876c35d6fd491
Vedant Aggarwal <vedantaggarwal27@gmail.com>
2018-05-16 13:52:45 +0530

    capture image

--- camera.py
+++ camera.py
@@ -5,6 +5,7 @@ import cv2
 import numpy as np
  stream = io.BytesIO()
 with picamera.PiCamera() as cam1:
+	cam1.resolution = (200,200)
 	cam1.start_preview()
 	time.sleep(2)
 	cam1.capture(stream , format = 'jpeg')
